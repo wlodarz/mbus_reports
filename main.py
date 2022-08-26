67ed176e35eb1e37b62418c51e385fb865e92643
W K <wlk@poczta.fm>
2022-08-26 16:53:44 +0200

    sending on the last month day

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
-from datetime import date
+import datetime
 from reckoning import *
 from db import *
 
 def main():
     config_filename = 'flats.xml'
-    today = date.today()
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
     day = int(today.strftime("%d"))
     month = int(today.strftime("%m"))
+    month_tomorrow = int(tomorrow.strftime("%m"))
     year = int(today.strftime("%Y"))
+
     reckoning = Reckoning(config_filename, day, month, year)
     reckoning.acquire_measures()
-    reckoning.generate_report()
-    reckoning.send_report()
+    if month != month_tomorrow:
+        reckoning.generate_report()
+        reckoning.send_report()
+        print('sending')
+    else:
+        print('not sending')
     data = reckoning.get_measures()
     db = DB('mbus', 'mbus', 'bukowa')
     db.connect()
